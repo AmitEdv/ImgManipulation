d1ff332a88c6a91683f84d3e9c099ffcaff21a5c
AmitEdv <amit.edvy@gmail.com>
2019-07-31 02:48:32 +0300

    add basic benchmark. organized some code.

--- GreenEyeAssignment/main.cpp
+++ GreenEyeAssignment/main.cpp
@@ -4,43 +4,71 @@
 
 using namespace cv;
 using namespace std;
+using namespace std::chrono;
 
-//Note! Please comment out the define in order to run production mode.
+//Comment out the define in order to run production mode.
 //debug mode consist additional logs and tests.
-//#define DEBUG_MODE
+//#define APP_DEBUG_MODE
+
+//Comment out the define in order to disable benchmark feature.
+//the feature measures the total running time of the application.
+#define APP_BENCHMARK
 
 static const int NUM_OF_IMGS = 10;
 static const string IMGS_FOLDER_PATH = "c:/greenEyeImgs/";
 static const string IMGS_DEFAULT_FORMAT = ".jpg";
 
-static const int RGB_IMG_NUM_OF_CHANNELS = 3;
-static const int RGB_CHANNEL_INDEX_BLUE = 0;
-static const int RGB_CHANNEL_INDEX_GREEN = 1;
-static const int RGB_CHANNEL_INDEX_RED = 2;
 static const double CHANNEL_FILTER_BLUE = 0.299;
 static const double CHANNEL_FILTER_GREEN = 0.587;
 static const double CHANNEL_FILTER_RED = 0.114;
 
-static vector<thread> g_threads;
-static const double g_channels_filters[RGB_IMG_NUM_OF_CHANNELS] = {CHANNEL_FILTER_BLUE, CHANNEL_FILTER_GREEN, CHANNEL_FILTER_RED};
+enum e_BGR_channel_index {
+	BGR_CHANNEL_INDEX_BLUE = 0,
+	BGR_CHANNEL_INDEX_GREEN,
+	BGR_CHANNEL_INDEX_RED,
+	BGR_IMG_NUM_OF_CHANNELS
+};
+
+vector<thread> g_threads;
+#ifdef APP_BENCHMARK
+time_point<system_clock> g_benchmarkStartTime;
+#endif //APP_BENCHMARK
+
+#ifdef APP_BENCHMARK
+inline void startBenchmark() {
+	g_benchmarkStartTime = high_resolution_clock::now();
+}
+#endif //APP_BENCHMARK
+
+#ifdef APP_BENCHMARK
+inline void stopBenchmarkAndPrintResult() {
+	time_point<system_clock> end = high_resolution_clock::now();;
+	long long elapsed_microseconds = std::chrono::duration_cast<microseconds>(end - g_benchmarkStartTime).count();
+	cout << "===============================" << endl;
+	cout << "Benchmark total run time:  " << elapsed_microseconds << " (usec)" << endl;
+	cout << "===============================" << endl;
+}
+#endif //APP_BENCHMARK
 
 double convert3ChannelsInto1(int blue, int green, int red) {
-	return ((g_channels_filters[RGB_CHANNEL_INDEX_BLUE] * blue)
-		+ (g_channels_filters[RGB_CHANNEL_INDEX_GREEN] * green)
-		+ (g_channels_filters[RGB_CHANNEL_INDEX_RED] * red));
+	static const double g_channelsFilters[BGR_IMG_NUM_OF_CHANNELS] = {
+		CHANNEL_FILTER_BLUE, CHANNEL_FILTER_GREEN, CHANNEL_FILTER_RED };
+
+	return ((g_channelsFilters[BGR_CHANNEL_INDEX_BLUE] * blue)
+		+ (g_channelsFilters[BGR_CHANNEL_INDEX_GREEN] * green)
+		+ (g_channelsFilters[BGR_CHANNEL_INDEX_RED] * red));
 }
 
 static Mat doSomething(Mat img) {
 	int imgWidth = img.size().width;
 	int imgHeight = img.size().height;
-#ifdef DEBUG_MODE
+#ifdef APP_DEBUG_MODE
 	cout << "Dims : " << img.dims << endl;
 	cout << "Width : " << imgWidth << endl;
 	cout << "Height: " << imgHeight << endl;
-#endif //DEBUG_MODE
-	if (img.channels() != RGB_IMG_NUM_OF_CHANNELS) {
+#endif //APP_DEBUG_MODE
+	if (img.channels() != BGR_IMG_NUM_OF_CHANNELS) {
 		cout << "Error - Image must be an RGB image!" << endl;
-		//TODO- handle error
 	}
 	
 	Mat grayValuesMatrix = Mat(imgHeight, imgWidth, CV_64F);
@@ -51,18 +79,18 @@ static Mat doSomething(Mat img) {
 	for (int i = 0; i < imgHeight; i++) {
 		for (int j = 0; j < imgWidth; j++) {
 			intensity = img.at<Vec3b>(i, j);
-			blue = intensity.val[RGB_CHANNEL_INDEX_BLUE];
-			green = intensity.val[RGB_CHANNEL_INDEX_GREEN];
-			red = intensity.val[RGB_CHANNEL_INDEX_RED];
-#ifdef DEBUG_MODE
+			blue = intensity.val[BGR_CHANNEL_INDEX_BLUE];
+			green = intensity.val[BGR_CHANNEL_INDEX_GREEN];
+			red = intensity.val[BGR_CHANNEL_INDEX_RED];
+#ifdef APP_DEBUG_MODE
 			cout << "blue = " << (int)blue << ", green = " << (int)green << ", red = " << (int)red << endl;
-#endif //DEBUG_MODE
+#endif //APP_DEBUG_MODE
 
 			double grayVal = convert3ChannelsInto1((int)blue, (int)green, (int)red);
 
-#ifdef DEBUG_MODE
+#ifdef APP_DEBUG_MODE
 			cout << grayVal << endl;
-#endif //DEBUG_MODE
+#endif //APP_DEBUG_MODE
 			grayValuesMatrix.at<double>(i, j) = grayVal;
 		}
 	}
@@ -81,22 +109,24 @@ static void applyGrayscaleFilterOnImage(string imgFullPath) {
 	Mat filterdImg = doSomething(img);
 	
 	imwrite(imgFullPath, filterdImg);
-#ifdef DEBUG_MODE
+#ifdef APP_DEBUG_MODE
 	imshow("Image", filterdImg);
-#endif //DEBUG_MODE
+#endif //APP_DEBUG_MODE
 	cout << imgFullPath << " done" << endl;
 }
 
 int main() {
 	static const int EXIT_WITH_ERROR;
 	
-	cout << "LEGEN -waitforit- ..." << endl;
+#ifdef APP_BENCHMARK
+	startBenchmark();
+#endif //APP_BENCHMARK
 	
 	for (int i = 0; i < NUM_OF_IMGS; i++) {
 		string fullImgPathStr = IMGS_FOLDER_PATH + to_string(i) + IMGS_DEFAULT_FORMAT;
-#ifdef DEBUG_MODE
+#ifdef APP_DEBUG_MODE
 		cout << "image " << fullImgPathStr << endl;
-#endif //DEBUG_MODE
+#endif //APP_DEBUG_MODE
 		g_threads.push_back(thread(applyGrayscaleFilterOnImage, fullImgPathStr));
 	}
 
@@ -104,7 +134,9 @@ int main() {
 		g_threads[i].join();
 	}
 	
-	cout << "DARY!" << endl;
+#ifdef APP_BENCHMARK
+	stopBenchmarkAndPrintResult();
+#endif //APP_BENCHMARK
 
 	waitKey(0);
 }
